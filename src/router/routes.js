9276b3e865cfe4eb0b5ff814a896649ccb6ec217
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2022-12-13 10:24:45 +0000

    removing Sunburst route

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,5 @@ export const HomeView = () => import('@/views/Home/HomeView.vue');
 export const ResourceTypeView = () => import('@/views/Resource/ResourceTypeView.vue');
 export const SubjectTypeView = () => import('@/views/Subject/SubjectTypeView.vue');
 export const DomainTypeView = () => import('@/views/Domain/DomainTypeView.vue');
-export const SunBurstView = () => import('@/views/SunBurstView.vue');
 
 
