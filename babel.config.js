e207d3170a016afe0c7faa86fcb4301ef630dcf0
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2022-12-20 13:56:12 +0000

    deleted package-lock.json file

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     "@vue/cli-plugin-babel/preset",
+    ['@babel/preset-env', {targets: {node: 'current'}}],
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
