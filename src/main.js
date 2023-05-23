4effb828feb1ca5867ca5d1c75df11f23189d33c
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-05-23 10:49:24 +0100

    removing am5 import

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import vuetify from './plugins/vuetify'
 import store from './store'
 import Particles from "vue2-particles";
 import VueSanitize from "vue-sanitize";
-import * as am5 from '@amcharts/amcharts5'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'core-js/stable'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
@@ -13,7 +12,6 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 Vue.config.productionTip = false
 Vue.use(Particles);
 Vue.use(VueSanitize);
-Vue.use(am5);
 
 new Vue({
   render: (h) => h(App),
