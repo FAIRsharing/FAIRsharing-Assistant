82da597db0f5bf0e3cfed97d3d0ea7adf906d94a
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-06-22 12:25:12 +0100

    fixing build error

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,7 +63,12 @@ module.exports = {
   },
   parser: "vue-eslint-parser",
   parserOptions: {
-    parser: "babel-eslint"
+    parser: '@babel/eslint-parser',
+    babelOptions: {
+      parserOpts: {
+        plugins: ["jsx"]
+      }
+    }
   },
   overrides: [
     {
