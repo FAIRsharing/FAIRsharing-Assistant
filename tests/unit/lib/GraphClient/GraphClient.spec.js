8fad11073273a5d211fd773b856a32e29de14045
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2023-06-29 13:30:01 +0100

    adding tests for GRAPHClient.js

---
+++ tests/unit/lib/GraphClient/GraphClient.spec.js
@@ -0,0 +1,101 @@
+import Client from "@/lib/GraphClient/GraphClient.js"
+import query from "./getRecords.json"
+
+const sinon = require("sinon");
+const axios = require("axios");
+
+describe("GraphQL Client", function () {
+  let client;
+  const searchFairsharingRecords = {records: [{}]};
+  let postStub = sinon.stub(axios, "post");
+  const stubQuery = {};
+  let localQuery = {};
+
+  beforeEach(() => {
+    localQuery = Object.assign(stubQuery, query);
+  });
+
+  beforeAll(() => {
+    client = new Client();
+    postStub.withArgs(sinon.match.any).returns({
+      data: {
+        data: {
+          searchFairsharingRecords
+        }
+      }
+    });
+  });
+
+  afterAll(() => {
+    postStub.restore()
+  });
+
+  it("can be instantiated as a singleton", function () {
+    process.env.VUE_APP_CLIENT_ID = undefined;
+    const instance2 = new Client();
+    expect(client).toBe(instance2.constructor["_instance"]);
+  });
+
+  it("can execute a query", async function () {
+
+    let stub = sinon.stub(Client.prototype, "getData");
+    stub.withArgs(sinon.match.any).returns({
+      data: {
+        data: {
+          searchFairsharingRecords
+        }
+      }
+    });
+
+    localQuery.queryParam = {};
+    localQuery.queryParam["fairsharingRegistry"] = "Standard";
+    localQuery.queryParam["isRecommended"] = true;
+    let output = await client.executeQuery(localQuery);
+    expect(JSON.stringify(output)).toBe(JSON.stringify({
+      searchFairsharingRecords: {
+        records: [{}]
+      }
+    }));
+
+    let output2 = await client.getData(localQuery);
+    expect(JSON.stringify(output2.data)).toBe(JSON.stringify({
+      data: {
+        searchFairsharingRecords: {
+          records: [{}]
+        }
+      }
+    }));
+    stub.restore();
+
+  });
+
+  it("can check either function returns errors or correct response ", async function () {
+    let stub = sinon.stub(Client.prototype, "getData");
+    localQuery.queryParam = {};
+    localQuery.queryParam["fairsharingRegistry"] = "Standard";
+    localQuery.queryParam["isRecommended"] = true;
+
+    stub.withArgs(sinon.match.any).returns({
+      data: {
+        errors: [
+          {message: "Im an error"}
+        ]
+      }
+    });
+    await expect(client.executeQuery(localQuery)).rejects;
+    stub.withArgs(sinon.match.any).returns({
+      data: {
+        data: {
+          searchFairsharingRecords
+        }
+      }
+    });
+
+    await expect(client.executeQuery(localQuery)).resolves.toStrictEqual({
+      searchFairsharingRecords: {
+        records: [{}]
+      }
+    });
+    Client.prototype.getData.restore();
+  });
+});
