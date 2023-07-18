c7dab09efab8be4de12ff11728abc236ba7e4614
Milo Thurston <knirirr@gmail.com>
2023-07-18 09:22:12 +0100

    Attempted to fix tests.

--- tests/unit/components/AllTags/EditTags.spec.js
+++ tests/unit/components/AllTags/EditTags.spec.js
@@ -3,35 +3,37 @@ import EditTags from "@/components/AllTags/EditTags.vue"
 import Vuetify from "vuetify"
 import Vuex from "vuex";
 import icons from "@/plugins/icons";
-//import multiTagsStore from "@/store/multiTagsStore";
+import GraphClient from "@/lib/GraphClient/GraphClient"
+import sinon from "sinon";
+import multiTagsStore from "@/store/multiTagsStore";
+import multiTagsFilter from "@/lib/GraphClient/queries/multiTagsFilter/multiTagsFilter.json";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
-
-
 let getters, store;
 
 // TODO: This appears to not provide the required stores in the components.
+// TODO: i.e. `[vuex] module namespace not found in mapGetters(): multiTagsStore/`
 
-getters = {
-  multiTagsStore: () => {
+multiTagsStore.getters = {
+  getFairSharingRecords: () => {
+    return [
+      {
+        record: "Wibble",
+        registry: "Standard",
+        id: 123,
+      }
+    ]  
+  },
+  getQueryParams:  () => {
     return {
-      getFairSharingRecords: [
-        {
-          record: "Wibble",
-          registry: "Standard",
-          id: 123,
-        }
-      ],
-      getQueryParams:  {
-        subjects: ['genetics'],
-        registry: ['standard']
-      },
-      getRefinedStatus: true,
-      getCurrentRegistry: 'standard',
-      getSelectedTags: []
+      subjects: ['genetics'],
+      registry: ['standard']         
     }
-  }
+  },
+  getRefinedStatus: () => { return true },
+  getCurrentRegistry: () => { return 'standard' },
+  getSelectedTags: () => { return [] }
 }
 
 store = new Vuex.Store({
@@ -42,6 +44,18 @@ const vuetify = new Vuetify({'icons':icons});
 
 describe("EditTags.vue", function(){
   let wrapper;
+  let graphStub;
+
+  graphStub = sinon.stub(GraphClient.prototype, "executeQuery");
+  graphStub.withArgs(multiTagsFilter).returns({
+    searchTags: [
+      { id: 1, label: 'label', model: 'subject' }
+    ]
+  })
+
+  afterAll(() => {
+    graphStub.restore();
+  })
 
   beforeEach(() => {
     wrapper = shallowMount(EditTags, {
@@ -54,5 +68,16 @@ describe("EditTags.vue", function(){
   it("can be instantiated", () => {
     expect(wrapper.vm.$options.name).toMatch("EditTags");
   });
+
+  /*
+  it("can get tags", async () => {
+    expect(wrapper.vm.recordTags).toStrictEqual([]);
+    let tags = await wrapper.vm.getTags('label');
+    console.log("TAGS: " + JSON.stringify(tags));
+    expect(wrapper.vm.tags).toStrictEqual([{id: 1, label: 'label', model: 'subject'}]);
+  });
+   */
+
+
 
 });
