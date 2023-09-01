843f74a2c23fca7cdd5668509af562a4fd8db19c
Milo Thurston <knirirr@gmail.com>
2023-09-01 13:56:16 +0100

    Added some more tests (from FAIRsharing).

---
+++ tests/unit/components/Results/RecordStatus.spec.js
@@ -0,0 +1,35 @@
+import {shallowMount} from "@vue/test-utils";
+import Vuetify from "vuetify"
+import RecordStatus from "@/components/Results/RecordStatus.vue"
+import light from '@/plugins/theme'
+
+
+let vuetify = new Vuetify({
+  theme: {
+    themes: {light}
+  }
+});
+
+describe("RecordStatus.vue", function () {
+  let wrapper;
+
+  wrapper = shallowMount(RecordStatus, {
+    vuetify,
+    propsData: {record: {status: 'ready', type: 'collection'}}
+  });
+
+  it("can be instantiated", () => {
+    expect(wrapper.name()).toMatch("RecordStatus");
+  });
+
+  /*
+  it("can check either record state as props is passed or not ", () => {
+    wrapper.setProps({record: {status: undefined, type: 'collection'}});
+    expect(wrapper.vm.statusStyles[wrapper.vm.record.status]).toStrictEqual({
+      title: '?',
+      tooltip: 'Undefined',
+      backColor: 'background: linear-gradient(red, red)'
+    });
+  });
+  */
+});
