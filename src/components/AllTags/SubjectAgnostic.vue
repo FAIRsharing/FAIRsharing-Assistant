78c5de78c5ddccc99e34f3e9f404bafa1ed8285d
Milo Thurston <knirirr@gmail.com>
2023-09-22 15:16:36 +0100

    Added link to subject browser in subject agnostic modal.

--- src/components/AllTags/SubjectAgnostic.vue
+++ src/components/AllTags/SubjectAgnostic.vue
@@ -15,19 +15,22 @@
       <v-card-subtitle
         style="color: white"
       >
-        Would you like to search for subject-agnostic resources as well?
+        Would you like to search for subject-agnostic resources as well? Alternatively, you might like to look at our
+        <a :href="subjectBrowserLink()" target="_blank">FAIRsharing Subject Browser</a> for alternative subject tags.
       </v-card-subtitle>
 
       <v-card-actions>
         <v-btn
+          small
           @click="yesPlease()"
         >
-          Yes please
+           Add subject agnostic
         </v-btn>
         <v-btn
+          small
           @click="noThanks()"
         >
-          No thanks
+          Don't add
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -63,17 +66,17 @@ export default {
       }
       if (_module.getCurrentRegistry) {
         const currentCount = _module.getFairSharingRecords.filter(x => x.registry === _module.getCurrentRegistry).length;
-        if (currentCount === 0) {
+        if (currentCount <= 2) {
           return true
         }
         return false;
       }
       else {
         let counts = [
-          _module.getFairSharingRecords.filter(x => x.registry === 'Standard').length === 0,
-          _module.getFairSharingRecords.filter(x => x.registry === 'Database').length === 0,
-          _module.getFairSharingRecords.filter(x => x.registry === 'Policy').length === 0,
-          _module.getFairSharingRecords.filter(x => x.registry === 'Collection').length === 0,
+          _module.getFairSharingRecords.filter(x => x.registry === 'Standard').length <= 2,
+          _module.getFairSharingRecords.filter(x => x.registry === 'Database').length <= 2,
+          _module.getFairSharingRecords.filter(x => x.registry === 'Policy').length <= 2,
+          _module.getFairSharingRecords.filter(x => x.registry === 'Collection').length <= 2,
         ]
         const callback = (element) => element === true;
         if (counts.some(callback) && Object.keys(_module.getQueryParams).length > 0) {
@@ -90,6 +93,17 @@ export default {
     },
     noThanks() {
       this.noThanksSelected = true;
+    },
+    subjectBrowserLink() {
+      const subjects = this.getQueryParams.subjects;
+      const defaultUrl = "https://fairsharing.org/browse/subject";
+      if (!subjects) {
+        return defaultUrl;
+      }
+      if (subjects.length === 1) {
+        return "https://fairsharing.org/browse/subject?term=" + subjects[0]
+      }
+      return defaultUrl;
     }
   }
 }
