5f46a4a1f1cfd338ce76a43fb590f9567981d2c8
Milo Thurston <knirirr@gmail.com>
2024-07-22 16:25:51 +0100

    Builds but does not run.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import store from './store'
-import Particles from "vue2-particles";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Vuetify from 'vuetify';
+import store from './store';
+import Particles from "@tsparticles/vue3";
 import VueSanitize from "vue-sanitize";
 //import SimpleAnalytics from "simple-analytics-vue";
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'core-js/stable'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 
+const vuetify = new Vuetify({});
 Vue.config.productionTip = false
 Vue.use(Particles);
 Vue.use(VueSanitize);
