3243625124cbe157825917d642021f008c518e14
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2024-11-05 12:11:59 +0000

    updating components/Results/KeywordTooltip.spec.js

--- tests/unit/components/Results/KeywordTooltip.spec.js
+++ tests/unit/components/Results/KeywordTooltip.spec.js
@@ -1,5 +1,9 @@
 import {shallowMount} from "@vue/test-utils";
+// import { createVuetify } from 'vuetify'
+import { describe, expect, it } from 'vitest'
 import KeywordTooltip from "@/components/Results/KeywordTooltip.vue"
+
+// const vuetify = createVuetify();
 
 describe('KeywordTooltip.vue', () => {
   let wrapper;
@@ -11,7 +15,7 @@ describe('KeywordTooltip.vue', () => {
   };
 
   it("can be mounted", () => {
-    wrapper = shallowMount(KeywordTooltip, { propsData: { keyword: keyword } });
+    wrapper = shallowMount(KeywordTooltip, { props: { keyword: keyword } });
     expect(wrapper.vm.$options.name).toMatch("KeywordTooltip");
     expect(wrapper.vm.processArray(wrapper.vm.keyword.synonyms)).toBe("abc, def: test.")
   });
@@ -21,4 +25,13 @@ describe('KeywordTooltip.vue', () => {
     wrapper = shallowMount(KeywordTooltip, { propsData: { keyword: keyword  } });
     expect(wrapper.vm.processArray(wrapper.vm.keyword.synonyms)).toBe(null);
   })
+
+  it ("can process without definitions in keyword object", () => {
+    keyword = {
+      label: 'abc',
+    };
+    wrapper = shallowMount(KeywordTooltip, { propsData: { keyword: keyword  } });
+    expect(wrapper.vm.capitaliseText(wrapper.vm.keyword['label'], 'taxonomy')).toBe("Abc");
+  })
+
 });
