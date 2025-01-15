4e739cd960c9d893dee698eceda7673478d09144
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2025-01-15 13:55:12 +0000

    blank commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,6 @@ dns.setDefaultResultOrder("verbatim");
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
-
 export default defineConfig({
   base: "./",
   server:{
